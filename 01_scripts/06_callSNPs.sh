8bfe48c7345f39f3037ac63b8f653e916b587d7b
bensutherland <bensuth@katak.ibis.ulaval.ca>
2015-04-17 09:14:04 -0400

    added calling SNPs

---
+++ 01_scripts/06_callSNPs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# first index fasta file, then combine position sorted aln files
+# then use mpileup to generate .vcf
+
+#Create a shell variable to store the location of our reference transcriptome
+REFERENCE=/project/lbernatchez/drobo/users/bensuth/03_RNASeq/SE-reads_assemble-to-counts/05_trinity_output/Trinity.fasta
+
+#index reference transcriptome
+samtools faidx $REFERENCE.fai
+
+#combine all RG-added sorted BAM files into one file
+cat ./07_mapped/*fastq.gz.bam > ./08_callSNPs/aln.bam
+
+samtools mpileup -uf $REFERENCE.fa ./08_callSNPs/aln.bam | \
+	bcftools view -bvcg - > ./08_callSNPs/var.raw.bcf
+bcftools view ./08_callSNPs/var.raw.bcf | vcfutils.pl varFilter -D100 > ./08_callSNPs/var.flt.vcf
