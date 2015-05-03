3e45bffdd5456cf30f17705674d4b84f88a62015
bensutherland <bensutherland7@gmail.com>
2015-05-03 13:23:01 -0400

    update1 to gene expr

--- 01_scripts/11_GXlevels.sh
+++ 01_scripts/11_GXlevels.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+#first convert ordered BAM back into SAM
 
- 
+#set environment variables
+MAPPED_FOLDER="07_mapped"
+COUNT_FOLDER="09_GXlevels"
+
+#convert sorted .bam back to .sam
+ls -1 $MAPPED_FOLDER/*fastq.gz.bam | \
+    sort -u | \ 
+    while read i
+    do
+        echo $i
+	samtools view -h "$i" > "$i".sam
+    done
+
+mv $MAPPED_FOLDER/*_counts.txt $COUNT_FOLDER/
