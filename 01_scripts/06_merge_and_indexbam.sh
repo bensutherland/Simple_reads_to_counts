756b497ad6797752e1e149c8f7d0807fdc046ac6
bensutherland <bensutherland7@gmail.com>
2015-05-06 09:08:02 -0400

    removed SNP scripts

--- 01_scripts/06_merge_and_indexbam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# merge all sorted bam files using read groups defined earlier into a single bam and index
-
-samtools merge -rh 00_archive/rg.txt ./08_callSNPs/merged.bam ./07_mapped/*.gz.bam
-samtools index ./08_callSNPs/merged.bam
-
-
-
