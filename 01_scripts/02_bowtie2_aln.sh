46b84642c395a6c8f7ceb1d7a9005b06349a0766
bensutherland <bensutherland7@gmail.com>
2017-03-08 09:23:19 -0500

    Add note about sort -n flag

--- 01_scripts/02_bowtie2_aln.sh
+++ 01_scripts/02_bowtie2_aln.sh
@@ -23,10 +23,10 @@ ls -1 $TRIMMED_FOLDER/*.fastq.gz |
 	  name=$(basename $i)
 	  label=$(echo $name | perl -pe 's/\_trimmed\.fastq\.gz//')
 	  ID="@RG\tID:${label}\tSM:${label}\tPL:Illumina"
-	  #bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE -U $i -S $i.bowtie2.sam
+	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE -U $i -S $i.bowtie2.sam
 	  samtools view -Sb $i.bowtie2.sam > $i.bowtie2.unsorted.bam
 	  samtools sort -n -o $i.bowtie2.sorted.bam $i.bowtie2.unsorted.bam
-	  samtools index $i.bowtie2.sorted.bam
+	  #samtools index $i.bowtie2.sorted.bam # note, if samtools sort -n is used, cannot index
 done
 
 # clean up space
