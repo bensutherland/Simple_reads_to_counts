b580e897567d03fba796741cd1c2ad9d165fab08
bensutherland <bensutherland7@gmail.com>
2022-03-20 14:39:22 -0700

    Update expected filenames from trimmomatic files and remove unsorted bam after sorting

--- 01_scripts/02_hisat2_aln_PE_to_stringtie.sh
+++ 01_scripts/02_hisat2_aln_PE_to_stringtie.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Map paired-end trimmed reads to reference genome with bowtie2 
+# Map trimmed, paired reads to a reference genome with hisat2 
 # Note: Requires that reference is indexed (see README.md) 
+# Note: Currently requires that input reads are *R[12].paired.fq.gz
+# Note: works with samtools htslib versions
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
 # Copy script as it was run
@@ -13,14 +15,19 @@ cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 # Global variables
 TRIMMED_FOLDER="03_trimmed"
 MAPPED_FOLDER="04_mapped"
-REFERENCE="/hdd/genomes/GCF_002872995.1_Otsh_v1.0_genomic.fna"
+REFERENCE="/scratch2/bsutherland/ref_genomes/GCF_902806645.1_cgigas_uk_roslin_v1_genomic"
 
 # User variables
-NUM_THREADS="8"
+NUM_THREADS="24"
 
 # Map reads and add RG
-ls -1 $TRIMMED_FOLDER/*.paired.fastq.gz |
-	perl -pe 's/\_[12]\.paired\.fastq\.gz//' |
+ls -1 $TRIMMED_FOLDER/*.paired.fq.gz |
+
+	# remove the read 1 or 2 suffix
+        sed 's/\_R1\.paired\.fq\.gz//g' | 
+	sed 's/\_R2\.paired\.fq\.gz//g' |	
+        
+        # map per sample 
 	sort -u |
 	while read i
 	do
@@ -28,12 +35,16 @@ ls -1 $TRIMMED_FOLDER/*.paired.fastq.gz |
 	  name=$(basename $i)
 	  label=$(echo $name)
 	  ID="@RG\tID:${label}\tSM:${label}\tPL:Illumina"
-	  hisat2 -x $REFERENCE -k 40 --threads $NUM_THREADS --rg-id $ID -1 $i"_1.paired.fastq.gz" -2 $i"_2.paired.fastq.gz" -S $i.hisat2.sam
+	  hisat2 -x $REFERENCE -k 40 --threads $NUM_THREADS --rg-id $ID -1 $i"_R1.paired.fq.gz" -2 $i"_R2.paired.fq.gz" -S $i.hisat2.sam
 	  samtools view -Sb $i.hisat2.sam > $i.hisat2.unsorted.bam
 	  samtools sort -o $i.hisat2.sorted.bam $i.hisat2.unsorted.bam
-	  #samtools index $i.bowtie2.sorted.bam # note, if samtools sort -n is used, cannot index
-      rm $i.hisat2.sam
-done
 
-# Move output bam files into the mapped folder
+	  # Remove the sam file and unsorted bam to save space
+          rm $i.hisat2.sam
+	  rm $i.hisat2.unsorted.bam
+
+        done
+
+# Move output sorted bam files into the mapped folder
 mv $TRIMMED_FOLDER/*.bam $MAPPED_FOLDER
+
