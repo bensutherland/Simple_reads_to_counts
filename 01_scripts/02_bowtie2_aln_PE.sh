1892ce9f568554376ba2fcb47878a0bbb6bfa335
bensutherland <bensutherland7@gmail.com>
2022-03-24 08:22:03 -0700

    Expect fq.gz name type (pipeline trimmed output)

--- 01_scripts/02_bowtie2_aln_PE.sh
+++ 01_scripts/02_bowtie2_aln_PE.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Map paired-end trimmed reads to reference genome with bowtie2 
 # Note: Requires that reference is indexed (see README.md) 
+# Note: Requires that input fastq files end in .fq.gz
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
 # Copy script as it was run
@@ -13,14 +14,19 @@ cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 # Global variables
 TRIMMED_FOLDER="03_trimmed"
 MAPPED_FOLDER="04_mapped"
-REFERENCE="/home/ben/Documents/genomes/GCF_002872995.1_Otsh_v1.0_genomic.fna"
+REFERENCE="/scratch2/bsutherland/ref_txomes/all_unigenes_155.fasta"
 
 # User variables
-NUM_THREADS="6"
+NUM_THREADS="24"
 
 # Map reads and add RG
-ls -1 $TRIMMED_FOLDER/*.paired.fastq.gz |
-	perl -pe 's/\_[12]\.paired\.fastq\.gz//' |
+ls -1 $TRIMMED_FOLDER/*.paired.fq.gz |
+	
+	# remove the read 1 or 2 suffix
+        sed 's/\_R1\.paired\.fq\.gz//g' |
+        sed 's/\_R2\.paired\.fq\.gz//g' |
+
+	# map per sample
 	sort -u |
 	while read i
 	do
@@ -28,12 +34,13 @@ ls -1 $TRIMMED_FOLDER/*.paired.fastq.gz |
 	  name=$(basename $i)
 	  label=$(echo $name)
 	  ID="@RG\tID:${label}\tSM:${label}\tPL:Illumina"
-	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE -1 $i"_1.paired.fastq.gz" -2 $i"_2.paired.fastq.gz" -S $i.bowtie2.sam
+	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE -1 $i"_R1.paired.fq.gz" -2 $i"_R2.paired.fq.gz" -S $i.bowtie2.sam
 	  samtools view -Sb $i.bowtie2.sam > $i.bowtie2.unsorted.bam
 	  samtools sort -n -o $i.bowtie2.sorted.bam $i.bowtie2.unsorted.bam
-	  #samtools index $i.bowtie2.sorted.bam # note, if samtools sort -n is used, cannot index
       rm $i.bowtie2.sam
+      rm $i.bowtie2.unsorted.bam
 done
 
 # Move output bam files into the mapped folder
 mv $TRIMMED_FOLDER/*.bam $MAPPED_FOLDER
+
