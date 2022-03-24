9009dfa7c7d931b5c6b4cc32837e4b2435057e31
bensutherland <bensutherland7@gmail.com>
2022-03-24 08:20:52 -0700

    Rename and minor edit SE alignment script

--- 01_scripts/02_bowtie2_aln_SE.sh
+++ 01_scripts/02_bowtie2_aln_SE.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Map trimmed reads to reference genome with bowtie2 
 # Note: Requires that reference is indexed (see README.md) 
+# Note: Requires that input fastq files end in .fq.gz
+
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+
+# Copy script as it was run
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="10_log_files"
+
+cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 # Global variables
 TRIMMED_FOLDER="03_trimmed"
@@ -11,7 +21,7 @@ REFERENCE="/home/ben/Documents/genomes/GCF_002872995.1_Otsh_v1.0_cds_from_genomi
 NUM_THREADS="4"
 
 # Map reads and add RG
-ls -1 $TRIMMED_FOLDER/*.fastq.gz |
+ls -1 $TRIMMED_FOLDER/*.fq.gz |
 	sort -u |
 	while read i
 	do
@@ -22,9 +32,10 @@ ls -1 $TRIMMED_FOLDER/*.fastq.gz |
 	  bowtie2 --end-to-end -k 40 --threads $NUM_THREADS --rg-id $ID -x $REFERENCE -U $i -S $i.bowtie2.sam
 	  samtools view -Sb $i.bowtie2.sam > $i.bowtie2.unsorted.bam
 	  samtools sort -n -o $i.bowtie2.sorted.bam $i.bowtie2.unsorted.bam
-	  #samtools index $i.bowtie2.sorted.bam # note, if samtools sort -n is used, cannot index
       rm $i.bowtie2.sam
+      rm $i.bowtie2.unsorted.bam
 done
 
 # clean up space
 mv ./$TRIMMED_FOLDER/*.bam ./$MAPPED_FOLDER/
+
