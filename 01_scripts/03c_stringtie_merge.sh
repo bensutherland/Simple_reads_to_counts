3e5852fe6f2bdc8ad8634b0be81540192b524f55
bensutherland <bensutherland7@gmail.com>
2022-10-11 13:35:55 -0700

    Hard code reference folder

--- 01_scripts/03c_stringtie_merge.sh
+++ 01_scripts/03c_stringtie_merge.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-# Use ref genome gtf to create known and unannotated output gtf for each sample
+# Use ref genome GFF to create known and unannotated output GTF for each sample
 # Note: Requires sorted .bam 
-TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
-# Copy script as it was run
-SCRIPT=$0
-NAME=$(basename $0)
-LOG_FOLDER="10_log_files"
-
-cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
-
-# Variables
+# Global variables
 MAPPED_FOLDER="04_mapped"
-REFERENCE_GFF="/scratch2/bsutherland/ref_genomes/GCF_902806645.1_cgigas_uk_roslin_v1_genomic.gff"
+REF_FOLDER="10_reference"
 MERGELIST="00_archive/mergelist.txt"
 NUM_THREADS="1" # seems to only use one thread
 
-# Run Stringtie merge 
-stringtie --merge -p $NUM_THREADS -G $REFERENCE_GFF -o $MAPPED_FOLDER/stringtie_merged.gtf $MERGELIST 
+# User variables
+REFERENCE_GFF="GCF_902806645.1_cgigas_uk_roslin_v1_genomic.gff"
+
+# Run StringTie merge 
+stringtie --merge -p $NUM_THREADS -G $REF_FOLDER/$REFERENCE_GFF \
+	  -o $MAPPED_FOLDER/stringtie_merged.gtf \
+	  $MERGELIST 
 
