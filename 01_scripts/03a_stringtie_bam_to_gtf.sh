966405c5d45b98134fa0e177398bbce5071662df
bensutherland <bensutherland7@gmail.com>
2022-10-11 08:14:08 -0700

    Clean up initial stringtie script

--- 01_scripts/03a_stringtie_bam_to_gtf.sh
+++ 01_scripts/03a_stringtie_bam_to_gtf.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 # Use ref genome gff to create known and unannotated output gtf for each sample
 # Note: Requires sorted .bam 
-TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
-
-# Copy script as it was run
-SCRIPT=$0
-NAME=$(basename $0)
-LOG_FOLDER="10_log_files"
-
-cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 # Global variables
 MAPPED_FOLDER="04_mapped"
-REFERENCE_GFF="/scratch2/bsutherland/ref_genomes/GCF_902806645.1_cgigas_uk_roslin_v1_genomic.gff"
+REFERENCE_GFF="10_reference/GCF_902806645.1_cgigas_uk_roslin_v1_genomic.gff"
 
 # User variables
-NUM_THREADS="20"
+NUM_THREADS="16"
 
-# Run Stringtie 
+# Run Stringtie on each sorted bam file 
 ls -1 $MAPPED_FOLDER/*.sorted.bam |
 	perl -pe 's/\.hisat2\.sorted\.bam//' |
 	sort -u |
@@ -29,5 +21,6 @@ ls -1 $MAPPED_FOLDER/*.sorted.bam |
 
           # Run stringtie per sample
 	  stringtie -p $NUM_THREADS -G $REFERENCE_GFF -o $i".gtf" -l $label $i".hisat2.sorted.bam" 
+
 done
 
