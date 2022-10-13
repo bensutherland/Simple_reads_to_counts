55992b1e9b09d0a089760057f7c97217e96e98d3
bensutherland <bensutherland7@gmail.com>
2022-10-13 11:41:00 -0700

    Remove sample list prior to generating sample list again

--- 01_scripts/05_build_sample_list.sh
+++ 01_scripts/05_build_sample_list.sh
@@ -6,6 +6,9 @@
 MAPPED_FOLDER="04_mapped"
 GXLEVELS_FOLDER="05_gx_levels"
 
+# Remove any previous instances of sample_list
+rm 00_archive/sample_list.txt
+
 # Use the GTF filenames in the mapped folder to create a sample list
 ls -1 $MAPPED_FOLDER/*.gtf |
         grep -vE 'stringtie_merged.gtf' |
